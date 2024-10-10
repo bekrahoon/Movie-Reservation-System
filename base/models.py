a367314b4c8d8c85a033f85dc7a1e599973b8527
Bekrahoon Umurjanov <bekrahoon12@gmail.com>
2024-10-10 18:19:26 +0600

    delete: add_movie.html, edit_movie.html, delete_movie.html
    fix: models.py, urls.py,views.py

--- base/models.py
+++ base/models.py
@@ -18,7 +18,9 @@ class Movie(BaseModel):
     poster = models.ImageField(upload_to="poster/")
     genre = models.ManyToManyField(Genre)
     show_time = models.DateTimeField()
-
+    price = models.DecimalField(max_digits=6, decimal_places=2)  
+    available_seats = models.PositiveIntegerField() 
+    
     def __str__(self):
         return self.title
 
@@ -27,14 +29,15 @@ class Booking(BaseModel):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE)
     show_time = models.DateTimeField()
-    seats = models.IntegerField()
+    seats = models.PositiveIntegerField()
+    booked_at = models.DateTimeField(default=timezone.now())
     
     def can_cancel(self):
         # Проверка, что бронирование можно отменить только до начала сеанса
         return self.show_time > timezone.now()
 
     def __str__(self):
-        return f"{self.user.username} - {self.movie.title} - {self.show_time}"
+        return f"{self.user.username} - {self.movie.title} - {self.show_time}  - {self.seats} seats"
 
     class Meta:
         constraints = [
--- views/views.py
+++ views/views.py
@@ -11,32 +11,6 @@ def home(request: HttpRequest):
     return render(request, "movies/home.html", context)
 
 
-@staff_member_required
-def add_movie(request):
-    if request.method == "POST":
-        form = MovieForm(request.POST, request.FILES)
-        if form.is_valid:
-            form.save()
-            messages.success(request, 'Фильм успешно добавлен.')
-            return redirect("home")
-    else:
-        form = MovieForm()
-    return render(request, "movies/add_movie.html", {"form": form})
-
-
-@staff_member_required
-def edit_movie(request, movie_id):
-    movie = get_object_or_404(Movie, id=movie_id)
-    if request.method == "POST":
-        form = MovieForm(request.Post, request.FILES, instance=movie)
-        if form.is_valid():
-            form.save()
-            return redirect("home")
-
-    else:
-        form = MovieForm(instance=movie)
-    return render(request, "movies/edit_movie.html", {"form": form, "movie":movie})
-
 def delete_movie(request, pk):
     movie = get_object_or_404(Movie, pk=pk)
     
@@ -51,17 +25,4 @@ def movie_detail(request, pk):
     return render(request, 'movies/movie_detail.html', {'movie': movie})
 
 
-@staff_member_required
-def admin_report(request):
-    bookings = Booking.objects.all()
-    
-    total_income = sum(booking.movie.price * booking.seats for booking in bookings)
-    
-    available_seats = {movie.title: movie.available_seats for movie in Movie.objects.all()}
-    
-    context = {
-        "bookings":bookings,
-        "total_income":total_income,
-        "available_seats":available_seats
-    }
-    return render(request, "admin/report.html", context)
+
