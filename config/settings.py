082f95ccf4c704b52b00ad159c8e09e8170182d8
Bekrahun <bekrahoon12@gmail.com>
2026-06-08 15:04:06 +0600

    ьmodified:   config/settings.py

--- config/settings.py
+++ config/settings.py
@@ -104,6 +104,16 @@ DATABASES = {
     }
 }
 
+# Allow using SQLite locally when a simple development run is desired.
+USE_SQLITE = config("USE_SQLITE", default=False, cast=bool)
+if USE_SQLITE:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
